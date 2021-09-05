e48fc8eb4621f0d48cbf7d60ecd110da1cabe366
Tetiana Perevozniuk <fidget16322@gmail.com>
2021-09-05 13:19:26 +0300

    feat: implement  GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.minVal;
+      this.maxVal;
+      this.guessVal;
+    }
+  
     setRange(min, max) {
-
+      this.minVal = min;
+      this.maxVal = max;
     }
-
+  
     guess() {
-
+      return (this.guessVal = Math.round((this.minVal + this.maxVal) / 2));
     }
-
+  
     lower() {
-
+      return (this.maxVal = this.guessVal);
     }
-
+  
     greater() {
-
+      return (this.minVal = this.guessVal);
     }
-}
+  }
 
 module.exports = GuessingGame;
